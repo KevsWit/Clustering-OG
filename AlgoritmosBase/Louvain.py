ae29106003f6e5ad78a4d7f34cc2b4b423d8a54c
Kevin Castillo <96481791+KevsWit@users.noreply.github.com>
2024-10-13 02:06:03 -0500

    Test Clustering Louvain
    Prueba de la clusterización Louvain en los grafos de prueba

--- AlgoritmosBase/Louvain.py
+++ AlgoritmosBase/Louvain.py
@@ -2,16 +2,31 @@ import networkx as nx
 import community.community_louvain as community_louvain
 import matplotlib.pyplot as plt
 
-# Crear el grafo del club de karate de Zachary
-G = nx.karate_club_graph()
+# Función para aplicar Louvain y graficar
+def aplicar_louvain_y_graficar(G, title):
+    partition = community_louvain.best_partition(G)
+    pos = nx.spring_layout(G)
+    cmap = plt.get_cmap('viridis')
+    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=300, cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    nx.draw_networkx_labels(G, pos)
+    plt.title(title)
+    plt.show()
 
-# Aplicar el algoritmo de Louvain
-partition = community_louvain.best_partition(G)
 
-# Dibujar el grafo con las comunidades detectadas
-pos = nx.spring_layout(G)
-cmap = plt.get_cmap('viridis')
-nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=300, cmap=cmap, node_color=list(partition.values()))
-nx.draw_networkx_edges(G, pos, alpha=0.5)
-nx.draw_networkx_labels(G, pos)
-plt.show()
+
+# Grafo del club de karate de Zachary
+G_karate = nx.read_gml('test\\karate.gml')
+aplicar_louvain_y_graficar(G_karate, "Karate Club")
+
+# Grafo de Dolphins
+G_dolphins = nx.read_gml('test\\dolphins.gml')
+aplicar_louvain_y_graficar(G_dolphins, "Dolphins")
+
+# Grafo de Political Books
+G_pol_books = nx.read_gml('test\\polbooks.gml')
+aplicar_louvain_y_graficar(G_pol_books, "Political Books")
+
+# Grafo de Football
+G_football = nx.read_gml('test\\football.gml')
+aplicar_louvain_y_graficar(G_football, "Football")
